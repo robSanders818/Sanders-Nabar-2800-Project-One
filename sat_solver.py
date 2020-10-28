b91aed7266d79eee969f60e154440ad720e82840
Ohm Nabar <ohm.nabar@gmail.com>
2020-10-28 22:47:58 +0000

    Made ClassScheduler a Class inside of sat_solver

--- sat_solver.py
+++ sat_solver.py
@@ -4,27 +4,21 @@ from pysat.solvers import Glucose3
 from class_data import ClassData
 from section import Section
 
-var_count = 0
-class_ref = {}
-section_ref = {}
-solver = Glucose3()
+class ClassScheduler:
+    def __init__(self):
+        self.var_count = 0
+        self.class_ref = {}
+        self.section_ref = {}
+        self.solver = Glucose3()
 
-def add_cardinality_constraint(variables: List, constraint : int):
-    print("lol")
-
-def sat_solver():
-    global var_count; global class_ref; global section_ref;
-
-    with open('classes.csv', encoding='utf-8-sig') as classes_file:
-        classes_list = [line.split(',') for line in classes_file]
-        for class_desc in classes_list:
-            class_temp = ClassData(class_desc[0], class_desc[1], class_desc[2][:-1])
-            class_ref[class_temp.subject + " " + class_temp.class_id] = class_temp
-    with open('sections.csv', encoding='utf-8-sig') as sections_file:
-        sections_list = [line.split(',') for line in sections_file]
-        for sections_desc in sections_list:
-            section_temp = Section(int(sections_desc[0]), sections_desc[2] + " " + sections_desc[1], int(sections_desc[3][:-1]))
-            section_ref[section_temp.section_id] = section_temp
-            var_count += 1
-if __name__ == '__main__':
-    sat_solver()
+        with open('classes.csv', encoding='utf-8-sig') as classes_file:
+            classes_list = [line.split(',') for line in classes_file]
+            for class_desc in classes_list:
+                class_temp = ClassData(class_desc[0], class_desc[1], class_desc[2][:-1])
+                self.class_ref[class_temp.subject + " " + class_temp.class_id] = class_temp
+        with open('sections.csv', encoding='utf-8-sig') as sections_file:
+            sections_list = [line.split(',') for line in sections_file]
+            for sections_desc in sections_list:
+                section_temp = Section(int(sections_desc[0]), sections_desc[2] + " " + sections_desc[1], int(sections_desc[3][:-1]))
+                self.section_ref[section_temp.section_id] = section_temp
+                self.var_count += 1
