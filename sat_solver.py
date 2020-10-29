f1ec475943f8d681b1ba2381bd7e92c786d8653b
Ohm Nabar <ohm.nabar@gmail.com>
2020-10-29 00:07:02 +0000

    Cardinality and section constraints

--- sat_solver.py
+++ sat_solver.py
@@ -10,6 +10,7 @@ class ClassScheduler:
         self.class_ref = {}
         self.section_ref = {}
         self.solver = Glucose3()
+        self.going = False
 
         with open('classes.csv', encoding='utf-8-sig') as classes_file:
             classes_list = [line.split(',') for line in classes_file]
@@ -22,3 +23,51 @@ class ClassScheduler:
                 section_temp = Section(int(sections_desc[0]), sections_desc[2] + " " + sections_desc[1], int(sections_desc[3][:-1]))
                 self.section_ref[section_temp.section_id] = section_temp
                 self.var_count += 1
+    
+    def go(self):
+        if self.going:
+            print("Already running!")
+            return
+        self.going = True
+        self.add_cardinality_constraint(list(self.section_ref.keys()), 4) #hard encoding a maximum of 4 classes
+
+    def solve(self):
+        if not self.going:
+            print("Scheduler not running! Start scheduler with go()")
+            return
+
+        if self.solver.solve():
+            print("Satisfiable!")
+            for section_id in self.solver.get_model():
+                if section_id in self.section_ref.keys():
+                    section_temp = self.section_ref[section_id]
+                    print(section_temp.class_name + " at " + str(section_temp.time) + ":00, section_id: " + str(section_id))
+        else:
+            print("Unsatisfiable :(")
+
+    def add_cardinality_constraint(self, variables: List[int], card: int):
+        if not variables or card < 1:
+            return
+
+        var_offset = self.var_count
+        self.var_count += (len(variables) * card)
+        aux_var = lambda i, j : var_offset + (i * card) + j
+        
+        self.solver.add_clause([-variables[0], aux_var(0, 1)])
+        for j in range(2, card + 1):
+            self.solver.add_clause([-aux_var(0, j)])
+
+        for i in range(1, len(variables)):
+            self.solver.add_clause([ -variables[i], aux_var(i, 1) ])
+            self.solver.add_clause([ -aux_var(i - 1, 1), aux_var(i, 1) ])
+            self.solver.add_clause([ -aux_var(i - 1, card), -variables[i] ])
+            for j in range(2, card + 1):
+                self.solver.add_clause([ aux_var(i, j), -variables[i], -aux_var(i - 1, j - 1)])
+                self.solver.add_clause([ aux_var(i, j), -aux_var(i - 1, j) ])
+
+    def add_section_constraint(self, section_id :int):
+        if section_id not in self.section_ref.keys():
+            print("Invalid section")
+            return
+        self.solver.add_clause([ section_id ])
+    
