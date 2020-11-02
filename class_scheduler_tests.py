3ee8179f83ef9e023df034eddeed5e468478d750
Robert Sanders <45695926+robSanders818@users.noreply.github.com>
2020-11-01 23:33:53 -0500

    Update class_scheduler_tests.py

--- class_scheduler_tests.py
+++ class_scheduler_tests.py
@@ -7,6 +7,7 @@ class ClassSchedulerTests(TestCase):
 
     def test_all(self):
         self.test_five_class_constraint(ClassScheduler())
+        self.test_five_class_constraint(ClassScheduler())
         print('Passed all Tests')
 
     def test_five_class_constraint(self, c: ClassScheduler):
@@ -16,3 +17,17 @@ class ClassSchedulerTests(TestCase):
         c.add_class_constraint('CS 1800')
         c.solve()
         self.assertTrue(c.do_solver() == 'Satisfiable!\nCS 2500 at 7:00, section_id: 1\nCS 1800 at 9:00, section_id: 7')
+        c.add_class_constraint('CS 1800')
+        c.solve()
+        self.assertTrue(c.do_solver() == 'Satisfiable!\nCS 2500 at 7:00, section_id: 1\nCS 1800 at 9:00, section_id: 7\nMATH 2331 at 8:00, section_id: 21')
+        c.add_class_constraint('PHIL 1111')
+        c.solve()
+        self.assertTrue(c.do_solver() == 'Satisfiable!\nCS 2500 at 7:00, section_id: 1\nCS 1800 at 9:00, section_id: 7\nMATH 2331 at 8:00, section_id: 21\nPHIL 1111 at 15:00, section_id: 45')
+        c.add_class_constraint('ECON 1116')
+        c.solve()
+        self.assertTrue(c.do_solver() == 'Unsatisfiable :(')
+
+
+
+
+
