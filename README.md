6c47aa9a6c082512d418bbc4f28cc57f669c3430
Robert Sanders <45695926+robSanders818@users.noreply.github.com>
2020-11-02 00:10:57 -0500

    Added README/ class scheduler tests

---
+++ README.md
@@ -0,0 +1,61 @@
+# Midterm Project: Class Scheduler Satisfiability Problem
+## Ohm Nabar and Robert Sanders
+
+### Results:
+It was really interesting to analyze the output of our Class Scheduler along 
+different constraints, and combinations of classes.  We set out a goal to 
+find an available schedule when provided with a dataset of classes.  We
+successfully completed our goal, as will be demonstrated here.  The sat solver
+finds a valid schedule based on the users desired subjects, classes,
+timeframe, and nupath constraints. Here we'll walk through some 
+examples of the class scheduler to see the behavior it follows.
+ 
+![](images/add%20class%20constraint.PNG)
+
+In this example we add a full schedule of classes, choosing sections
+with different starting times, and of different classes, and as we see
+The full schedule is satisfiable.  Then we add 1 more class (ECON
+1116), which shouldn't work because we can only take 4 classes, and as
+we see the Class Scheduler returns that it is unsatisfiable! 
+ 
+![](images/section%20constraint%20unsatisfiable.PNG)
+
+This example, adds the first section which is at 7:00, and then adds the 6th section which is also
+at 7:00.  This is a contradiction, as we cannot take two classes at the same time, and appropriately so
+the Class Scheduler informs the user that this combination of classes is unsatisfiable
+
+![](images/subject%20constraint%20unsatisfiable.PNG)
+
+This example evaluates adding in constraints for each subject.  This finds a minimum satisfiable section
+to fit from that subject.  As we see trying to add in a 5th subject would not work as you can't take
+5 classes.
+
+![](images/add_nupath_constraint.PNG)
+
+This example adds various nu path constraints, with the scheduler choosing the minimum possible class 
+to create a satisfiable schedule.  Here adding a 5th nupath constraint causes an error, as a student
+can't take 5 classes.
+
+![](images/unsatisfiable%20time%20constraint.PNG)
+
+In this example we evalutate time constraints, as this student wants to only take classes between 10:00 
+and 16:00.  We try selecting section number 1 for them, which is a 7:00 class, and doesn't fit in their 
+time constraints.  Therefore this schedule is unsatisfiable.
+
+![](images/time%20with%20class%20constraint.PNG)
+
+In this example we evaluate a full schedule of a student with strict time constraints.  The desired
+schedule times are from 10:00 to 16:00.  We add the sections 8 and 13.  Then we want to take two more
+classes, so we choose those class constraints.  The scheduler chooses sections so as to not interfere
+with the prior sections 8 and 13.
+
+![](images/subject%20constraint%20in%20limited%20time.PNG)
+
+In this schedule, we set the same time constraints of 10:00 to 16:00, and add 3 exact class constraints.
+We then add Philosophy as a subject to find a section of philosophy to fit within those time constraints
+and it successfully chooses a generic philosophy class this student could fit into their schedule.
+
+![](images/no%20time%20satisfiability.PNG)
+
+In this schedule, we show how a schedule with only 3 hours of time constraints is unsatisfiable when 
+choosing 4 classes, as we couldn't choose 4 sections without time overlap.
--- class_scheduler_tests.py
+++ class_scheduler_tests.py
@@ -1,13 +1,31 @@
 from unittest import TestCase
 from sat_solver import ClassScheduler
 
+
 class ClassSchedulerTests(TestCase):
+    """
+    Class performing tests for various combinations of uses for scheduler
+    """
     def __init__(self) -> None:
         self.test_all()
 
     def test_all(self):
         self.test_five_class_constraint(ClassScheduler())
-        self.test_five_class_constraint(ClassScheduler())
+        print(1)
+        self.test_same_time_sections(ClassScheduler())
+        print(2)
+        self.test_subject_constraints(ClassScheduler())
+        print(3)
+        self.test_nupath_constraints(ClassScheduler())
+        print(4)
+        self.test_unsat_time_constraint(ClassScheduler())
+        print(5)
+        self.test_time_with_class(ClassScheduler())
+        print(6)
+        self.test_subject_constraint_limited(ClassScheduler())
+        print(7)
+        self.test_no_time_sat(ClassScheduler())
+        print(8)
         print('Passed all Tests')
 
     def test_five_class_constraint(self, c: ClassScheduler):
@@ -15,17 +33,95 @@ class ClassSchedulerTests(TestCase):
         c.add_class_constraint('CS 2500')
         self.assertTrue(c.do_solver() == 'Satisfiable!\nCS 2500 at 7:00, section_id: 1')
         c.add_class_constraint('CS 1800')
-        c.solve()
         self.assertTrue(c.do_solver() == 'Satisfiable!\nCS 2500 at 7:00, section_id: 1\nCS 1800 at 9:00, section_id: 7')
         c.add_class_constraint('CS 1800')
-        c.solve()
         self.assertTrue(c.do_solver() == 'Satisfiable!\nCS 2500 at 7:00, section_id: 1\nCS 1800 at 9:00, section_id: 7\nMATH 2331 at 8:00, section_id: 21')
         c.add_class_constraint('PHIL 1111')
-        c.solve()
         self.assertTrue(c.do_solver() == 'Satisfiable!\nCS 2500 at 7:00, section_id: 1\nCS 1800 at 9:00, section_id: 7\nMATH 2331 at 8:00, section_id: 21\nPHIL 1111 at 15:00, section_id: 45')
         c.add_class_constraint('ECON 1116')
-        c.solve()
         self.assertTrue(c.do_solver() == 'Unsatisfiable :(')
+
+    def test_same_time_sections(self, c: ClassScheduler):
+        c.go()
+        c.add_section_constraint(1)
+        self.assertTrue(c.do_solver() == 'Satisfiable!\nCS 2500 at 7:00, section_id: 1')
+        c.add_section_constraint(6)
+        self.assertTrue(c.do_solver() == 'Unsatisfiable :(')
+
+    def test_subject_constraints(self, c: ClassScheduler):
+        c.go()
+        c.add_subject_constraint('CS')
+        self.assertTrue(c.do_solver() == 'Satisfiable!\nCS 2500 at 7:00, section_id: 1')
+        c.add_subject_constraint('PHIL')
+        self.assertTrue(c.do_solver() == 'Satisfiable!\nCS 2500 at 7:00, section_id: 1\nPHIL 1111 at 8:00, section_id: 41')
+        c.add_subject_constraint('MATH')
+        self.assertTrue(c.do_solver() == 'Satisfiable!\nCS 2500 at 7:00, section_id: 1\nMATH 2800 at 15:00, section_id: 36\nPHIL 1111 at 8:00, section_id: 41')
+        c.add_subject_constraint('ECON')
+        self.assertTrue(c.do_solver() == 'Satisfiable!\nCS 2500 at 7:00, section_id: 1\nMATH 2800 at 15:00, section_id: 36\nPHIL 1111 at 8:00, section_id: 41\nECON 1116 at 10:00, section_id: 65')
+        c.add_subject_constraint('THTR')
+        self.assertTrue(c.do_solver() == 'Unsatisfiable :(')
+
+    def test_nupath_constraints(self, c: ClassScheduler):
+        c.go()
+        c.add_subject_constraint('1')
+        self.assertTrue(c.do_solver() == 'Satisfiable!\nCS 2500 at 7:00, section_id: 1')
+        c.add_subject_constraint('2')
+        self.assertTrue(c.do_solver() == 'Satisfiable!\nCS 2500 at 7:00, section_id: 1\nCS 3200 at 9:00, section_id: 18')
+        c.add_subject_constraint('3')
+        self.assertTrue(c.do_solver() == 'Satisfiable!\nCS 2500 at 7:00, section_id: 1\nCS 3200 at 9:00, section_id: 18\nTHTR 1000 at 15:00, section_id: 80')
+        c.add_subject_constraint('4')
+        self.assertTrue(c.do_solver() == 'Satisfiable!\nCS 2500 at 7:00, section_id: 1\nCS 3200 at 9:00, section_id: 36\nTHTR 1000 at 15:00, section_id: 41\nTHTR 1433 at 10:00, section_id: 81')
+        c.add_subject_constraint('5')
+        self.assertTrue(c.do_solver() == 'Unsatisfiable :(')
+
+    def test_unsat_time_constraint(self, c: ClassScheduler):
+        c.go()
+        c.add_time_constraint(10, 16)
+        self.assertTrue(c.do_solver() == 'Satisfiable!')
+        c.add_section_constraint(1)
+        self.assertTrue(c.do_solver() == 'Unsatisfiable :(')
+
+    def test_time_with_class(self, c: ClassScheduler):
+        c.go()
+        c.add_time_constraint(10, 16)
+        c.add_section_constraint(8)
+        self.assertTrue(c.do_solver() == 'Satisfiable!\nCS 1800 at 11:00, section_id: 8')
+        c.add_section_constraint(13)
+        self.assertTrue(c.do_solver() == 'Satisfiable!\nCS 1800 at 11:00, section_id: 8\nCS 2800 at 15:00, section_id: 13')
+        c.add_class_constraint('PHIL 1111')
+        self.assertTrue(c.do_solver() == 'Satisfiable!\nCS 1800 at 11:00, section_id: 8\nCS 2800 at 15:00, section_id: 13\nPHIL 1111 at 10:00, section_id: 42')
+        c.add_class_constraint('ECON 1000')
+        self.assertTrue(c.do_solver() == 'Satisfiable!\nCS 1800 at 11:00, section_id: 8\nCS 2800 at 15:00, section_id: 13\nPHIL 1111 at 12:00, section_id: 42\nECON 1000 at 10:00, section_id: 61')
+
+    def test_subject_constraint_limited(self, c: ClassScheduler):
+        c.go()
+        c.add_time_constraint(10, 16)
+        c.add_class_constraint('CS 2500')
+        self.assertTrue(c.do_solver() == 'Satisfiable!\nCS 2500 at 11:00, section_id: 3')
+        c.add_class_constraint('CS 1800')
+        self.assertTrue(c.do_solver() == 'Satisfiable!\nCS 2500 at 11:00, section_id: 3\nCS 1800 at 13:00, section_id: 9')
+        c.add_class_constraint('CS 2800')
+        self.assertTrue(c.do_solver() == 'Satisfiable!\nCS 2500 at 11:00, section_id: 3\nCS 1800 at 13:00, section_id: 9\nCS 2800 at 12:00, section_id: 12')
+        c.add_subject_constraint('PHIL')
+        self.assertTrue(c.do_solver() == 'Satisfiable!\nCS 2500 at 11:00, section_id: 3\nCS 1800 at 13:00, section_id: 9\nCS 2800 at 12:00, section_id: 12\nPHIL 1115 at 15:00, section_id: 49')
+
+    def test_no_time_sat(self, c: ClassScheduler):
+        c.go()
+        c.add_time_constraint(10, 12)
+        c.add_section_constraint(3)
+        self.assertTrue(c.do_solver() == 'Satisfiable!\nCS 2500 at 11:00, section_id: 3')
+        c.add_section_constraint(22)
+        self.assertTrue(c.do_solver() == 'Satisfiable!\nCS 2500 at 11:00, section_id: 3\nMATH 2331 at 10:00, section_id: 22')
+        c.add_section_constraint(31)
+        self.assertTrue(c.do_solver() == 'Satisfiable!\nCS 2500 at 11:00, section_id: 3\nMATH 2331 at 10:00, section_id: 22\nMATH 4581 at 12:00, section_id: 31')
+        c.add_subject_constraint('PHIL')
+        self.assertTrue(c.do_solver() == 'Unsatisfiable :(')
+
+
+
+
+
+
 
 
 
